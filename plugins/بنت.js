00bfe4e4451354e71b82168110f2d1490e56a16f
Moaaz232 <168116418+Moaaz232@users.noreply.github.com>
2024-06-30 23:02:25 +0300

    Update بنت.js

--- plugins/بنت.js
+++ plugins/بنت.js
@@ -1,10 +1,10 @@
 import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix, command }) => {
-let res = await fetch('https://api.waifu.pics/sfw/waifu')
+let res = await fetch('')
 if (!res.ok) throw await res.text()
 let json = await res.json()
 if (!json.url) throw `${lenguajeGB['smsAvisoFG']()}`
-conn.sendFile(m.chat, json.url, 'error.jpg', `شايفك يالي عاوز تتجوزها 🗿💔`, m)
+conn.sendFile(m.chat, json.url, 'error.jpg', `شايفك حرام 🗿💔`, m)
 //conn.sendButton(m.chat, `😻😻😻😻`, wm, json.url, [['𝙎𝙄𝙂𝙐𝙄𝙀𝙉𝙏𝙀 | 𝙉𝙀𝙓𝙏 🆕', `/${command}`]], m)
 }
 handler.help = ['waifu']
